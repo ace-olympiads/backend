b72cc35d9d5242e3c5b0cb59c4da2f86b4caa191
Sahajpreet Singh <sp.code2003@gmail.com>
2023-06-11 07:36:42 +0530

    rfac: remove cache files

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
