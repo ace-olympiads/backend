b478c383616822c423f65cc41076ce86d4c5c82b
Surya-Phour <s_phour@ee.iitr.ac.in>
2023-06-25 12:58:23 +0530

    feat: add deploy config

--- backend/settings.py
+++ backend/settings.py
@@ -44,13 +44,8 @@ DEBUG = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = [
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
-    ]
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',             
