2ecf51b0ec771e21f2a1a70c74fddbf172d7091a
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2023-08-20 11:44:21 +0530

    feat: Add user commets get route

--- question/urls.py
+++ question/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ExaminationListAPIView, QuestionByExaminationView, QuestionByTagView, QuestionListCreateView, QuestionRetrieveUpdateDestroyView, CommentsView, SearchAPIView, TagListAPIView
+from .views import ExaminationListAPIView, QuestionByExaminationView, UserCommentsView,QuestionByTagView, QuestionListCreateView, QuestionRetrieveUpdateDestroyView, CommentsView, SearchAPIView, TagListAPIView
 
 urlpatterns = [
     path('add/', QuestionListCreateView.as_view(), name='question-list-create'),
     path('<int:question_id>/', QuestionRetrieveUpdateDestroyView.as_view(), name='question-retrieve-update-destroy'),
     path('comments/<int:id>/', CommentsView.as_view(), name='comments'),
     path('comments/', CommentsView.as_view(), name='comments'),
+    path('user-comments/<str:email>/', UserCommentsView.as_view(), name='user-comments'),
     path('tag/<int:tag_id>/', QuestionByTagView.as_view(), name='question-by-tag'),
     path('tags/', TagListAPIView.as_view(), name='get-all-tags'),
     path('search/', SearchAPIView.as_view(), name='search'),
