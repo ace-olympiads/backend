40ab3c02ed801f5c4bdc19280c9ca1b6f123862d
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2023-12-15 12:50:43 +0530

    fix: cors2

--- backend/settings.py
+++ backend/settings.py
@@ -49,8 +49,8 @@ CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://aceacad.com"
-    "https://backend.aceacad.com"
+    "https://aceacad.com",
+    "https://backend.aceacad.com",
     "http://photon0205.pythonanywhere.com",
     "http://thuglyf0205.pythonanywhere.com",
     "https://ace-olympiads-frontend-ommanimesh2.vercel.app"
